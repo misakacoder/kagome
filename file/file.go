905d1950f5a630b7fa810f7091556aab8f3e25a1
misakacoder <210086826@qq.com>
2025-12-31 17:26:06 +0800

    init

--- file/file.go
+++ file/file.go
@@ -1,7 +1,8 @@
-package os
+package file
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func ExistFile(filepath string) bool {
@@ -12,4 +13,9 @@ func ExistFile(filepath string) bool {
 func ExistDir(directory string) bool {
 	info, err := os.Stat(directory)
 	return err == nil && info.IsDir()
+}
+
+func WriteFile(filename string, data []byte) {
+	os.Mkdir(filepath.Dir(filename), os.ModePerm)
+	os.WriteFile(filename, data, os.ModePerm)
 }
--- maps/map.go
+++ maps/map.go
@@ -9,3 +9,20 @@ type Map[K comparable, V any] interface {
 	Clear()
 	Range(func(K, V))
 }
+
+func MergeMap(src, dst map[string]any) map[string]any {
+	for key, srcValue := range src {
+		if dstValue, ok := dst[key]; ok {
+			srcValueMap, srcValueMapOk := srcValue.(map[string]any)
+			dstValueMap, dstValueMapOk := dstValue.(map[string]any)
+			if srcValueMapOk && dstValueMapOk {
+				dst[key] = MergeMap(srcValueMap, dstValueMap)
+			} else {
+				dst[key] = srcValue
+			}
+		} else {
+			dst[key] = srcValue
+		}
+	}
+	return dst
+}
